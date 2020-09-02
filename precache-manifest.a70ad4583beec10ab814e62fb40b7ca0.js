dec54270d1be5e7439cd8304ffec26b400fc2e4c
LeeEugene1 <uwm1004@gmail.com>
2020-09-02 22:45:40 +0900

    Updates

---
+++ precache-manifest.a70ad4583beec10ab814e62fb40b7ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d1d3898604783cb3d037ea03f004d5",
+    "url": "/SassStudy/index.html"
+  },
+  {
+    "revision": "f7042a5dd42ebbb36611",
+    "url": "/SassStudy/static/css/main.09a722d6.chunk.css"
+  },
+  {
+    "revision": "6fdc41a3b53b9e0e0bd4",
+    "url": "/SassStudy/static/js/2.0495b993.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SassStudy/static/js/2.0495b993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7042a5dd42ebbb36611",
+    "url": "/SassStudy/static/js/main.55cb3c5b.chunk.js"
+  },
+  {
+    "revision": "5d4aa6bea71a6d2d8576",
+    "url": "/SassStudy/static/js/runtime-main.23ec8674.js"
+  }
+]);
