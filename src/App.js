3c00fa2bd340688579994e3068376d9955e2b4fe
LeeEugene1 <uwm1004@gmail.com>
2020-09-04 23:24:31 +0900

    button costumize and handing events width ...rest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./Button.scss"
 import Button from './Button';
+import "./App.scss"
 
 function App() {
   return (
     <div className="App">
-      <Button>
+      <Button size="large" color="grey" outline={true} className="costomized">
+        버튼입니다
+      </Button>
+      <Button size="medium" outline fullwidth className="costomized" onClick={() => {console.log('클릭!')}}>
         버튼입니다
       </Button>
     </div>
--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./Button.scss"
+import classNames from 'classnames';
 
-export default function Button({children}){
-return <button className="Button">{children}</button>
+export default function Button({children, size, color, outline, fullwidth, className, ...rest }){
+    // console.log(rest);
+return <button 
+    className={classNames(
+    'Button', 
+    size, 
+    color, 
+    {
+        outline, fullwidth
+    },
+    className
+    )} 
+    {...rest}>{children}</button>
 }
