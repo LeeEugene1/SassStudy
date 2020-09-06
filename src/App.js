83a8d032f58007a62f3120f86f61abac5365d16c
LeeEugene1 <uwm1004@gmail.com>
2020-09-06 23:29:49 +0900

    checkbox and how to use styled-components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from 'react';
-import "./Button.scss"
-import Button from './Button';
-import "./App.scss"
+// import "./Button.scss"
+// import Button from './Button';
+// import "./App.scss"
+// import CheckBox from './CheckBox';
+import styled, {css} from 'styled-components';
+
+const Circle = styled.div`
+  width:5rem; 
+  height:5rem; 
+  // backgroud:black;
+  background:${props => props.color};
+  ${props => props.huge && 
+  css`
+    width:10rem;
+    height:10rem;
+  `
+  }
+`;
 
 function App() {
+  // const [checked, setCheck] = useState(false);
+  // const onChange = (e) =>{
+  //   setCheck(e.target.checked);
+  // }
   return (
-    <div className="App">
+      /* 버튼 스타일링 */
+    /*<div className="App">
       <Button size="large" color="grey" outline={true} className="costomized">
         버튼입니다
       </Button>
       <Button size="medium" outline fullwidth className="costomized" onClick={() => {console.log('클릭!')}}>
         버튼입니다
       </Button>
-    </div>
+    </div>*/
+
+    // 체크버튼
+    // <div>      
+    //   <CheckBox onChange={onChange} checked={checked}>다음 약관에 모두 동의</CheckBox>
+    // </div>
+    <>
+    <Circle color='blue'/>
+    <Circle color='pink' huge/>
+
+    </>
   );
 }
 
---
+++ src/CheckBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
+import styles from './CheckBox.module.css';
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles);
+export default function CheckBox({checked, children,...rest}){
+    return(
+        // <div className={styles.checkbox}>
+        <div className={cx('checkbox')}>
+            <label>
+                <input type="checkbox" checked={checked} {...rest}></input>
+                <div className={styles.icon}>{checked ? <MdCheckBox className={styles.checked}/> : <MdCheckBoxOutlineBlank/>}</div>
+            </label>
+            <span>
+                {children}
+            </span>
+        </div>
+    );
+}
